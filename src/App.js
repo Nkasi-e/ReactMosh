6084c01268317375f7d1105689bfac5992637e43
Nkasi-e <nkasijnr@gmail.com>
2022-09-28 16:13:52 +0100

    lifecycle

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
-import Counters from "./components/Counters";
+// import Counters from "./components/Counters";
 import Navbar from "./components/Navbar";
 
 class App extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
+  state = {};
 
   // Incremet button fn
 
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
+  handleIncrement = (counter) => {};
 
   //Reset button fn
-  handleReset = () => {
-    const counters = this.state.counters.map((count) => {
-      count.value = 0;
-      return count;
-    });
-    this.setState({ counters });
-  };
+  handleReset = () => {};
 
   //delete fn
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter(
-      (count) => count.id !== counterId
-    );
-    this.setState({ counters });
-  };
+  handleDelete = (counterId) => {};
 
   render() {
     return (
@@ -47,12 +23,12 @@ class App extends Component {
           totalCounters={this.state.counters.filter((c) => c.value > 0).length}
         />
         <header className="App-header">
-          <Counters
+          {/* <Counters
             counters={this.state.counters}
             onDelete={this.handleDelete}
             onIncrement={this.handleIncrement}
             onReset={this.handleReset}
-          />
+    />*/}
         </header>
       </div>
     );
