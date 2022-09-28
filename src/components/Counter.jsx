3561800a536e339f800c0310e16ee97d0e6f1172
Nkasi-e <nkasijnr@gmail.com>
2022-09-28 09:43:33 +0100

    class component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,50 +1,65 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const Counter = () => {
-  const [value, setValue] = useState(0);
-  const formatCOunt = () => {
-    return value === 0 ? `Null` : value;
+class Counter extends Component {
+  state = {
+    value: 0,
   };
-  const styles = {
-    fontSize: 15,
-    fontweight: "bold",
-  };
-
-  // Get Batch classes... Rendering classes dynamically
-  let getBatchClasses = () => {
-    let classes = "badge m-2 badge-";
-    classes += value === 0 ? "warning" : value <= 0 ? "danger" : "primary";
-    return classes;
-  };
-
-  const listItems = ["Magma", "React", "ts", "py"];
-
-  //...
-  return (
-    <div>
-      <button
-        className="btn btn-danger m-1 btn-sm"
-        onClick={() => setValue(value - 1)}
-      >
-        Decrement
-      </button>
-      <h1 style={styles} className={getBatchClasses()}>
-        {formatCOunt()}
-      </h1>
-      <button
-        className="btn btn-success m-1 btn-sm"
-        onClick={() => setValue(value + 1)}
-      >
-        Increment
-      </button>
-      {/*Rendering list */}
-      <ul>
-        {listItems.map((item, id) => (
-          <li key={id}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <h1>Class component</h1>
+      </div>
+    );
+  }
+}
 
 export default Counter;
+
+// const Counter = () => {
+//   const [value, setValue] = useState(0);
+//   const formatCOunt = () => {
+//     return value === 0 ? `Null` : value;
+//   };
+//   const styles = {
+//     fontSize: 15,
+//     fontweight: "bold",
+//   };
+
+//   // Get Batch classes... Rendering classes dynamically
+//   let getBatchClasses = () => {
+//     let classes = "badge m-2 badge-";
+//     classes += value === 0 ? "warning" : value <= 0 ? "danger" : "primary";
+//     return classes;
+//   };
+
+//   const listItems = ["Magma", "React", "ts", "py"];
+
+//   //...
+//   return (
+//     <div>
+//       <button
+//         className="btn btn-danger m-1 btn-sm"
+//         onClick={() => setValue(value - 1)}
+//       >
+//         Decrement
+//       </button>
+//       <h1 style={styles} className={getBatchClasses()}>
+//         {formatCOunt()}
+//       </h1>
+//       <button
+//         className="btn btn-success m-1 btn-sm"
+//         onClick={() => setValue(value + 1)}
+//       >
+//         Increment
+//       </button>
+//       {/*Rendering list */}
+//       <ul>
+//         {listItems.map((item, id) => (
+//           <li key={id}>{item}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Counter;
